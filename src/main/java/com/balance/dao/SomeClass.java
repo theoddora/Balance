b4535da07df54b037f4e47f6a80fd64519413b67
ttosheva <theoddora@abv.bg>
2016-11-21 15:52:16 +0200

    Dao and Model packages add

---
+++ src/main/java/com/balance/dao/SomeClass.java
@@ -0,0 +1,7 @@
+package com.balance.dao;
+
+/**
+ * Created by ttosheva on 21/11/2016.
+ */
+public class SomeClass {
+}
---
+++ src/main/java/com/balance/model/SomeClassPt2.java
@@ -0,0 +1,7 @@
+package com.balance.model;
+
+/**
+ * Created by ttosheva on 21/11/2016.
+ */
+public class SomeClassPt2 {
+}
