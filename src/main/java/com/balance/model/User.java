5a6a1703fa917fc94616d835b2eba3d24a047199
pgenev <petemail@abv.bg>
2016-11-23 09:41:48 +0200

    Logger added

--- src/main/java/com/balance/model/User.java
+++ src/main/java/com/balance/model/User.java
@@ -1,29 +1,58 @@
 package com.balance.model;
 
 
+import com.balance.exceptions.UserException;
 
 public class User {
 
+    private int id;
     private String userName;
     private String email;
     private String password;
     private String name;
 
+    public User(){
 
-    public void setUserName(String userName) {
-        this.userName = userName;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    public User(int id, String userName, String email, String name, String password) throws UserException {
+        setId(id);
+        setUserName(userName);
+        setEmail(email);
+        setName(name);
+        setPassword(password);
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public void setUserName(String userName) throws UserException {
+        if(userName != null && !userName.isEmpty())
+            this.userName = userName;
+        else
+            throw new UserException("Invalid username!");
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setPassword(String password) throws UserException {
+        if(password != null && !password.isEmpty())
+            this.password = password;
+        else
+            throw new UserException("Invalid password!");
+    }
+
+    public void setEmail(String email) throws UserException {
+        if(email != null && !email.isEmpty())
+            this.email = email;
+        else
+            throw new UserException("Invalid email!");
+    }
+
+    public void setName(String name) throws UserException {
+        if(name != null && !name.isEmpty())
+            this.name = name;
+        else
+            throw new UserException("Invalid name!");
+    }
+
+    public void setId(int id){
+        this.id = id;
     }
 
 
