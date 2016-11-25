2674a9b358c170a8dc0d24db8ba6c01c765f6977
ttosheva <theoddora@abv.bg>
2016-11-25 17:06:55 +0200

    User validated with Spring validation.

--- src/main/java/com/balance/controller/UserController.java
+++ src/main/java/com/balance/controller/UserController.java
@@ -2,14 +2,17 @@ package com.balance.controller;
 
 import com.balance.dao.UserDAO;
 import com.balance.model.User;
+import org.postgresql.util.PSQLException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.util.Map;
+import javax.validation.Valid;
 
 /**
  * Created by ttosheva on 23/11/2016.
@@ -22,11 +25,21 @@ public class UserController {
     @Autowired
     private UserDAO userDAO;
 
+    private static final String DUPLICATE_USERNAME = "User with that username/email already exists.";
+
     //register
     @RequestMapping(value = "/registration", method = RequestMethod.POST)
-    public String registerUser(User user) {
+    public String registerUser(@Valid User user, Errors errors, Model model) {
 
-        userDAO.createUser(user);
+        if (errors.hasErrors()) {
+            return "register";
+        }
+        try {
+            userDAO.createUser(user);
+        }catch (DuplicateKeyException e) {
+            model.addAttribute("errorMessage", DUPLICATE_USERNAME);
+            return "register";
+        }
 
         System.out.println("username: " + user.getUsername());
         System.out.println("password: " + user.getPassword());
--- src/main/java/com/balance/model/User.java
+++ src/main/java/com/balance/model/User.java
@@ -2,21 +2,36 @@ package com.balance.model;
 
 
 import com.balance.exceptions.UserException;
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotBlank;
+
+import javax.validation.constraints.*;
 
 public class User {
 
-    private int id;
+    private long id;
+
+    @NotBlank(message = "{username.empty}")
+    @Size(min = 4, max = 20, message = "{username.size}")
     private String username;
+
+    @NotBlank(message = "{email.empty}")
+    @Pattern(regexp = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])", message = "{email.valid}")
+    @Email()
     private String email;
+
+    @NotBlank(message = "{password.empty}")
+    @Size(min = 5, max = 100, message = "{password.size}")
     private String password;
+
+    @NotBlank(message = "{name.empty}")
+    @Size(min = 2, max = 20, message = "{name.size}")
     private String name;
 
     public User() {
-
     }
 
-    public User( String username, String email, String name, String password) throws UserException {
-
+    public User(String username, String email, String name, String password) throws UserException {
         setUsername(username);
         setEmail(email);
         setName(name);
@@ -24,39 +39,28 @@ public class User {
     }
 
     public void setUsername(String username) throws UserException {
-        if(username != null && !username.isEmpty())
-            this.username = username;
-        else
-            throw new UserException("Invalid username!");
+        this.username = username;
+
     }
 
     public void setPassword(String password) throws UserException {
-        if(password != null && !password.isEmpty())
-            this.password = password;
-        else
-            throw new UserException("Invalid password!");
+        this.password = password;
     }
 
     public void setEmail(String email) throws UserException {
-        if(email != null && !email.isEmpty())
-            this.email = email;
-        else
-            throw new UserException("Invalid email!");
+        this.email = email;
     }
 
     public void setName(String name) throws UserException {
-        if(name != null && !name.isEmpty())
-            this.name = name;
-        else
-            throw new UserException("Invalid name!");
+        this.name = name;
     }
 
-    public void setId(int id){
+    public void setId(long id) {
         this.id = id;
     }
 
     //getters
-    public int getId() {
+    public long getId() {
         return id;
     }
 
